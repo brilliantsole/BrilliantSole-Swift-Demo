1590052862e75d54b4ea2bceed649204f88f8e92
Zack <zack@ukaton.com>
2025-01-15 14:45:56 -0800

    Initial Commit

---
+++ BrilliantSoleSwiftDemo/BrilliantSoleSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrilliantSoleSwiftDemoApp.swift
+//  BrilliantSoleSwiftDemo
+//
+//  Created by Zack Qattan on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BrilliantSoleSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrilliantSoleSwiftDemoTests/BrilliantSoleSwiftDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  BrilliantSoleSwiftDemoTests.swift
+//  BrilliantSoleSwiftDemoTests
+//
+//  Created by Zack Qattan on 1/15/25.
+//
+
+import Testing
+
+struct BrilliantSoleSwiftDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
